f36b861357294c3b6f93b8fe76c408c95c359759
Taeil Kwak <taeil2@gmail.com>
2020-08-02 17:25:01 -0500

    added a few translations

--- src/components/pages/work/project/project.js
+++ src/components/pages/work/project/project.js
@@ -19,6 +19,8 @@ const dictionary = {
   wordpress: 'WordPress',
   php: 'php',
   aspnet: 'ASP.NET',
+  postgresql: 'PostgreSQL',
+  express: 'Express',
 
   valueProposition: 'Value Proposition',
   competitiveAnalysis: 'Competitive Analysis',
